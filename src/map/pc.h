42d06657515cc2c1b0b9ff6df3cb67a4d3e8fb13
Cocoa <Cocoa@auriga.moe.hm>
2011-05-17 00:00:00 +0900

    Patch 0869
    ・転生、1次、2次判定マクロの条件式が間違っている点を修正（pc.h）

--- src/map/pc.h
+++ src/map/pc.h
@@ -193,20 +193,20 @@ int pc_check_guild_skill_effective_range(struct map_session_data *sd);
 						((sd)->status.class_ >= PC_CLASS_LG_H && (sd)->status.class_ <= PC_CLASS_SC_H) ||	\
 						(sd)->status.class_ == PC_CLASS_RK2_H || (sd)->status.class_ == PC_CLASS_LG2_H ||	\
 						(sd)->status.class_ == PC_CLASS_RA2_H || (sd)->status.class_ == PC_CLASS_NC2_H ||	\
-						(sd)->status.class_ == PC_CLASS_RK3_H || (sd)->status.class_ >= PC_CLASS_RK4_H ||	\
-						(sd)->status.class_ == PC_CLASS_RK5_H || (sd)->status.class_ >= PC_CLASS_RK6_H)
+						(sd)->status.class_ == PC_CLASS_RK3_H || (sd)->status.class_ == PC_CLASS_RK4_H ||	\
+						(sd)->status.class_ == PC_CLASS_RK5_H || (sd)->status.class_ == PC_CLASS_RK6_H)
 
 #define pc_isbaby(sd)  (((sd)->status.class_ >= PC_CLASS_NV_B && (sd)->status.class_ <= PC_CLASS_SNV_B) ||	\
 						((sd)->status.class_ >= PC_CLASS_RK_B && (sd)->status.class_ <= PC_CLASS_NC2_B))
 
 #define pc_is1stclass(sd) (((sd)->s_class.job >= PC_JOB_SM && (sd)->s_class.job <= PC_JOB_TF) ||	\
-						   (sd)->s_class.job == PC_JOB_SNV || (sd)->s_class.job <= PC_JOB_TK ||	\
-						   (sd)->s_class.job == PC_JOB_GS || (sd)->s_class.job <= PC_JOB_NJ ||		\
+						   (sd)->s_class.job == PC_JOB_SNV || (sd)->s_class.job == PC_JOB_TK ||	\
+						   (sd)->s_class.job == PC_JOB_GS || (sd)->s_class.job == PC_JOB_NJ ||		\
 						   (sd)->s_class.job == PC_JOB_MB)
 
 #define pc_is2ndclass(sd) (((sd)->s_class.job >= PC_JOB_KN && (sd)->s_class.job <= PC_JOB_DC) ||	\
-						   (sd)->s_class.job == PC_JOB_SG || (sd)->s_class.job <= PC_JOB_SL ||		\
-						   (sd)->s_class.job == PC_JOB_DK || (sd)->s_class.job <= PC_JOB_DA)
+						   (sd)->s_class.job == PC_JOB_SG || (sd)->s_class.job == PC_JOB_SL ||		\
+						   (sd)->s_class.job == PC_JOB_DK || (sd)->s_class.job == PC_JOB_DA)
 
 #define pc_is3rdclass(sd) ((sd)->s_class.job >= PC_JOB_RK && (sd)->s_class.job <= PC_JOB_SC)
 
